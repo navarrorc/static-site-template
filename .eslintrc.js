c10aa554ab0b95c15e91824be257abf302ef770a
Roberto C. Navarro <rnavarro70@gmail.com>
2018-11-02 14:10:21 -0500

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,6 @@ module.exports = {
   rules: {
     "no-console": "off",
     indent: ["error", 2],
-    // "linebreak-style": [
-    //     "error",
-    //     "windows"
-    // ],
     quotes: ["error", "double"],
     semi: "off"
   }
