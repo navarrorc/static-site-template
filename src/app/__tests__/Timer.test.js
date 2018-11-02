d5f9bafc02c1567761ce53fe90ceb584210a2f3f
Roberto C Navarro <navarrorc@users.noreply.github.com>
2018-11-02 11:50:49 -0500

    remove unused files

--- src/app/__tests__/Timer.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { create } from "react-test-renderer";
-import Timer from "../components/Timer";
-
-test("snapshot", () => {
-  const c = create(<Timer />);
-  expect(c.toJSON()).toMatchSnapshot();
-});
--- src/app/components/Message.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Message extends Component {
-  render() {
-    return <div>Welcome to the {this.props.page} page!</div>;
-  }
-}
-
-export default Message;
